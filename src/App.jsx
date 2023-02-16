32c4dd68857ad06dc0b07a9965b8572d64903ab2
Jaime Garcia <jaime.garcia@mrm.com>
2023-02-16 19:05:45 +0100

    01_01b

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 function App() {
-  return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+    const name = 'StarGazersss';
+    return (
+        <div className='container'>
+            <article>
+                <hgroup>
+                    <img src='images/group.svg' alt='' />
+                    <h1>
+                        Meet the <em style={{ color: 'steelblue' }}>{name}</em>
+                    </h1>
+                    <p>
+                        Members of an <b>intergalactic alliance</b> paving the way for peace and
+                        benevolence among all species. They are known for their enthusiasm for
+                        science, for their love of fun, and their dedication to education.
+                    </p>
+                    <button className='outline' onClick={() => console.log('Hi Mom!')}>
+                        click me
+                    </button>
+                </hgroup>
+            </article>
+        </div>
+    );
 }
-export default App
+export default App;
